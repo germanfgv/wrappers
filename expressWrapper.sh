bd5d6f27133bb285c80d44c79c5c9d2132ce3f65
andresfelquintero <41698961+andresfelquintero@users.noreply.github.com>
2019-08-21 11:40:14 -0500

    Updated expresswrapper
    Ditched github stuff.

--- expressWrapper.sh
+++ expressWrapper.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-
-echo This EXPRESS wrapper works by creating a new project area, so it should be used on a local lxplus area. If you already have a project area on the folder then this wrapper will not work
-echo Please remember to have your name, email and github user configured. If you do not, then use the following commands to do it.
-echo git config --global user.name "First name Last name"
-echo git config --global user.email "<Your-Email-Address>"
-echo git config --global user.github "<Your-GitHub-Account-Username>"
-echo Do you have a github account linked with this session and a forked copy of cmssw master branch? Answer Y or N
-read varnam1
-if [ $varnam1 == 'Y' ]
-then
+    echo This wrapper works by creating a new project area, so it should be used on a local lxplus area. If you already have a project area on the folder then this wrapper will not work
     echo Please provide the run number i.e. 326607
     read runnum
     curl "https://cmsweb.cern.ch/t0wmadatasvc/prod/express_config?run=$runnum" > any4.json
@@ -19,10 +10,6 @@ then
     read streamnam
     echo Please provide the LFN i.e. /store/whatever
     read lfn
-else
-    echo Please configure your github account with this lxplus session and make a forked copy of cmssw repository
-    exit 0
-fi
 
 curl "https://cmsweb.cern.ch/t0wmadatasvc/prod/express_config?run=$runnum&stream=$streamnam" > any3.json
 scramarchj=`cat any3.json | jq -r '.result[].scram_arch'`
@@ -47,33 +34,23 @@ cd $cmsswj/src/
 #source cms environment variables
 eval `scramv1 runtime -sh`
 
-#add package of data processing
-# check if this can be configured to include another version
-git cms-addpkg Configuration/DataProcessing
-
-#create the project
-scram b
-cd Configuration/DataProcessing/test
-
-# runs promptreco
-eval `scramv1 runtime -sh`
 echo Is $cmsswj a CMSSW version superior to 11_0_0_pre1? Answer Y or N
 read varnam2
-#TODO insert if statements to check if there are alcarecos or not.
+
 if [ $varnam2 == 'Y' ]
 then
     if [ $alcaskim == 'null' ]
     then
-        python RunExpressProcessing.py --scenario=$scenarioj --raw --reco --fevt --dqm --global-tag $globaltagj --lfn=$lfn --nThreads=nthread
+        python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunExpressProcessing.py --scenario=$scenarioj --raw --reco --fevt --dqm --global-tag $globaltagj --lfn=$lfn --nThreads=nthread
     else
-        python RunExpressProcessing.py --scenario=$scenarioj --raw --reco --fevt --dqm --global-tag $globaltagj --lfn=$lfn --nThreads=nthread --alcarecos=$alcaskim
+        python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunExpressProcessing.py --scenario=$scenarioj --raw --reco --fevt --dqm --global-tag $globaltagj --lfn=$lfn --nThreads=nthread --alcarecos=$alcaskim
     fi
 else
     if [ $alcaskim == 'null' ]
     then
-        python RunExpressProcessing.py --scenario=$scenarioj --raw --reco --fevt --dqm --global-tag $globaltagj --lfn=$lfn
+        python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunExpressProcessing.py --scenario=$scenarioj --raw --reco --fevt --dqm --global-tag $globaltagj --lfn=$lfn
     else
-        python RunExpressProcessing.py --scenario=$scenarioj --raw --reco --fevt --dqm --global-tag $globaltagj --lfn=$lfn --alcarecos=$alcaskim
+        python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunExpressProcessing.py --scenario=$scenarioj --raw --reco --fevt --dqm --global-tag $globaltagj --lfn=$lfn --alcarecos=$alcaskim
     fi
 fi
 echo If you want to do cmsRun -e RunExpressProcessingCfg.py then you should move to the following folder
