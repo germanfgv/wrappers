cd172e12cd964cefdadd51a4e6f05c6009b9103d
andresfelquintero <41698961+andresfelquintero@users.noreply.github.com>
2019-08-21 11:46:33 -0500

    Updated recoWrapper.sh
    Ditched github stuff, created conditions for execution when physicis skims and alca skims are null.

--- recoWrapper.sh
+++ recoWrapper.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
 echo This wrapper works by creating a new project area, so it should be used on a local lxplus area. If you already have a project area on the folder then this wrapper will not work
-echo Please remember to have your name, email and github user configured. If you do not, then use the following commands to do it.
-echo git config --global user.name "First name Last name"
-echo git config --global user.email "<Your-Email-Address>"
-echo git config --global user.github "<Your-GitHub-Account-Username>"
-echo Do you have a github account linked with this session and a forked copy of cmssw master branch? Answer Y or N
-read varnam1
-if [ $varnam1 == 'Y' ]
-then
-    echo Please provide the run number i.e. 326607
+
+    echo Please provide the run number i.e. 323940
     read runnum
-    echo Please provide the dataset name i.e. AlCaPhiSym
+    echo Please provide the dataset name i.e. ZeroBias
     read datanam
     echo Please provide the LFN i.e. /store/whatever
     read lfn
-else
-    echo Please configure your github account with this lxplus session and make a forked copy of cmssw repository
-    exit 0
-fi
 
 curl "https://cmsweb.cern.ch/t0wmadatasvc/prod/reco_config?run=$runnum&primary_dataset=$datanam" > any.json
 scramarchj=`cat any.json | jq -r '.result[].scram_arch'`
@@ -45,23 +34,44 @@ cd $cmsswj/src/
 #source cms environment variables
 eval `scramv1 runtime -sh`
 
-#add package of data processing
-# check if this can be configured to include another version
-git cms-addpkg Configuration/DataProcessing
-
-#create the project
-scram b
-cd Configuration/DataProcessing/test
-
-# runs promptreco
-eval `scramv1 runtime -sh`
 echo Is $cmsswj a CMSSW version superior to 11_0_0_pre1? Answer Y or N
 read varnam2
 if [ $varnam2 == 'Y' ]
 then
-    python RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj --nThreads=nthread --lfn=$lfn --alcarecos=$alcaskim --PhysicsSkims=$physkim
+    if [[$alcaskim == 'null' && $physkim == 'null' ]]
+    then
+    python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj --nThreads=nthread --lfn=$lfn
+    else
+        if [ $alcaskim == 'null' ]
+        then
+        python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj --nThreads=nthread --lfn=$lfn --PhysicsSkims=$physkim
+        else
+        if [ $physkim = 'null' ]
+        then
+        python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj --nThreads=nthread --lfn=$lfn --alcarecos=$alcaskim
+        else
+        python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj --nThreads=nthread --lfn=$lfn --alcarecos=$alcaskim --PhysicsSkims=$physkim
+        fi
+        fi
+    fi
+
 else
-    python RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj --lfn=$lfn --alcarecos=$alcaskim --PhysicsSkims=$physkim
+ if [[ $alcaskim == 'null' && $physkim == 'null' ]]
+    then
+    python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj --lfn=$lfn
+    else
+        if [ $alcaskim == 'null' ]
+        then
+        python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj  --lfn=$lfn --PhysicsSkims=$physkim
+        else
+            if [ $physkim = 'null' ]
+            then
+            python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj --lfn=$lfn --alcarecos=$alcaskim
+            else
+            python $CMSSW_RELEASE_BASE/src/Configuration/DataProcessing/test/RunPromptReco.py --scenario=$scenarioj --reco --aod --miniaod --dqmio --global-tag $globaltagj --lfn=$lfn --alcarecos=$alcaskim --PhysicsSkims=$physkim
+            fi
+        fi
+    fi
 fi
 echo If you want to use cmsRun -e RunPromptRecoCfg.py then you should move to the following folder
 pwd
